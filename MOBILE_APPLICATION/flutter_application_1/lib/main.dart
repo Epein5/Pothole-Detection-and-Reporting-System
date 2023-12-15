0c4785a1e5166945ffa3135d61df09a7cfab0fdf
Epein5 <aayushgtm5@gmail.com>
2023-12-15 05:13:16 -0800

    Added Splash Screen

--- MOBILE_APPLICATION/flutter_application_1/lib/main.dart
+++ MOBILE_APPLICATION/flutter_application_1/lib/main.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home.dart';
 import 'package:flutter_application_1/views/livedetection.dart';
 import 'package:provider/provider.dart';
-
-
+import 'dart:async';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,7 +21,59 @@ class MyApp extends StatelessWidget {
       child: const MaterialApp(
         title: 'Object Detection App',
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: SplashScreen(),
+      ),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement<void, void>(
+          context,
+          MaterialPageRoute<void>(
+            builder: (BuildContext context) => const HomePage(),
+          ));
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF4A148C),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 100,
+          ),
+          Container(
+            height: 300,
+            decoration: const BoxDecoration(
+                color: Colors.white, shape: BoxShape.circle),
+            width: double.infinity,
+            child: Image.asset('./assets/logo.png'),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const Center(
+              child: Text(
+            'POTHOLE DETECTION and REPORTING SYSTEM',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+        ],
       ),
     );
   }
